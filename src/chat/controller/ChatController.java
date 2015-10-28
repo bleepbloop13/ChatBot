0b92b11692fc25dc8eabc62d04d325e31148efb4
bleepbloop13 <rbak9218@740s-pl-161.local>
2015-10-28 13:34:44 -0600

    Completed contentChecker method

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -12,26 +12,34 @@ public class ChatController
 {
 	private Chatbot roryBot;
 	private ChatDisplay display;
-	
+
 	public ChatController()
 	{
 		display = new ChatDisplay();
 		String user = display.getUserInput("What is your name?");
 		roryBot = new Chatbot(user);
 	}
-	
+
 	public void start()
 	{
 		display.displayText("Hello " + roryBot.getUserName());
 		chat();
 	}
-	
+
 	private void chat()
 	{
 		String textFromUser = display.getUserInput("Talk to the chatbot");
-		while(roryBot.lengthChecker(textFromUser))
+		
+		while (roryBot.lengthChecker(textFromUser))
 		{
-			textFromUser = display.getUserInput("wow " + textFromUser);
+			if(roryBot.contentChecker(textFromUser))
+			{
+				display.displayText("Wow, I had no idea you loved " + roryBot.getContent());
+			}
+				
+			textFromUser = display.getUserInput(textFromUser);
+		
 		}
+		
 	}
 }
